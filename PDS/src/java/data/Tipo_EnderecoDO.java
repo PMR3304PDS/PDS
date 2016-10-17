99d489d0a3b42cb892b6f6bae5525f57dc3a4d27
Cliente <Cliente@Cliente-VAIO>
2016-10-17 11:42:02 -0700

    Classes JAVA de interface com o banco (get e set)

---
+++ PDS/src/java/data/Tipo_EnderecoDO.java
@@ -0,0 +1,23 @@
+
+package data;
+
+public class Tipo_EnderecoDO {
+    private int _TipEnd_cod;
+private String _TipEnd_tipo;
+
+    public int getTipEnd_cod() {
+        return _TipEnd_cod;
+    }
+
+    public void setTipEnd_cod(int _TipEnd_cod) {
+        this._TipEnd_cod = _TipEnd_cod;
+    }
+
+    public String getTipEnd_tipo() {
+        return _TipEnd_tipo;
+    }
+
+    public void setTipEnd_tipo(String _TipEnd_tipo) {
+        this._TipEnd_tipo = _TipEnd_tipo;
+    }
+}
---
+++ PDS/src/java/data/Tipo_ExameDO.java
@@ -0,0 +1,26 @@
+
+package data;
+
+public class Tipo_ExameDO {
+    
+private int _TipExa_cod;
+private String _TipExa_descricao;
+
+    public int getTipExa_cod() {
+        return _TipExa_cod;
+    }
+
+    public void setTipExa_cod(int _TipExa_cod) {
+        this._TipExa_cod = _TipExa_cod;
+    }
+
+    public String getTipExa_descricao() {
+        return _TipExa_descricao;
+    }
+
+    public void setTipExa_descricao(String _TipExa_descricao) {
+        this._TipExa_descricao = _TipExa_descricao;
+    }
+
+  
+}
