7c203d67780f2091fc241c65540d86fb95f37fd7
aluno <aluno@192.168.198.129>
2016-10-31 16:01:23 -0200

    Upload MedicoDATA

---
+++ PDS/src/java/data/MedicoDATA.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.sql.*;
+import java.util.*;
+import utils.Transacao;
+
+public class MedicoDATA {
+
+  public void incluir(MedicoDO medico, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "insert into Medico (Conselhos_Con_cond, Estado_Est_cod_conselho_emissor, Med_NumRegistro) values (?, ?, ?)";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setInt(1, medico.getConselhos_Con_cond());
+     ps.setInt(2, medico.getEstado_Est_cod_conselho_emissor());
+     ps.setString(3, medico.getMed_NumRegistro());
+     int result = ps.executeUpdate();
+  }
+
+
+  public void atualizar(MedicoDO medico, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "update ComentarioExame set Estado_Est_cod_conselho_emissor=?, Med_NumRegistro=? where Conselhos_Con_cond=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setInt(1, medico.getEstado_Est_cod_conselho_emissor());
+     ps.setString(2, medico.getMed_NumRegistro());
+     ps.setInt(3, medico.getConselhos_Con_cond());
+     int result = ps.executeUpdate();
+  } // atualizar
+  
+  public MedicoDO buscar(int idobj, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "select * from Medico where Conselhos_Con_cond=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setInt(1, idobj);
+     ResultSet rs = ps.executeQuery();
+     rs.next();
+     MedicoDO medico = new MedicoDO();
+     medico.setConselhos_Con_cond(rs.getInt("Conselhos_Con_cond"));
+     medico.setEstado_Est_cod_conselho_emissor(rs.getInt("Estado_Est_cod_conselho_emissor"));
+     medico.setMed_NumRegistro(rs.getString("Med_NumRegistro"));
+     return medico;
+  } // buscar
+
+} // MedicoDATA
