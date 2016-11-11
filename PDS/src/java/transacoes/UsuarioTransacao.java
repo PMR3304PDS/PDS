4515fc57e7be6f144c59e7024a2f53848713317d
Thiago <Dell@DESKTOP-766BGUU>
2016-11-11 15:29:39 -0200

    Atualização 2 de UsuarioTransacao.java

--- PDS/src/java/transacoes/UsuarioTransacao.java
+++ PDS/src/java/transacoes/UsuarioTransacao.java
@@ -10,31 +10,7 @@ import java.util.*;
  */
 public class UsuarioTransacao {
    
-    public boolean incluir (UsuarioDO usuario) throws Exception{
-
-     // validacao das regras de negocio
-     if ( (isEmpty(usuario.getUsu_nome())) ) {
-       return false;
-     }
-
-     // efetuando a transacao
-     Transacao tr = new Transacao();
-     try {
-
-       tr.begin();
-         UsuarioData udata = new UsuarioData();
-         udata.incluir(usuario, tr);
-       tr.commit();
-       return true;
-       
-     } catch(Exception e) {
-         tr.rollback();
-         System.out.println("erro ao incluir " + usuario.getUsu_nome());
-         e.printStackTrace();
-     }
-     return false;
-  } // incluir
-    
+  
     public UsuarioDO buscar(String user_name) throws Exception {
         Transacao tr = new Transacao();
             try {
@@ -51,12 +27,5 @@ public class UsuarioTransacao {
             return null;
     }
     
-    private boolean isEmpty(String s) {
-     if (null == s)
-       return true;
-     if (s.length() == 0)
-       return true;
-     return false;
-  }
     
 }
