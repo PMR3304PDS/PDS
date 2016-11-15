f96b129be7e41be5c0d985459b6a119a98aa9db5
Renan <Renan@10.211.55.3>
2016-11-15 18:57:17 -0200

    transições pra um usuário

--- PDS/src/java/transacoes/Usuario.java
+++ PDS/src/java/transacoes/Usuario.java
@@ -9,12 +9,61 @@ import java.util.*;
  * @author Renan
  */
 public class Usuario{
+    
     public UsuarioDO buscar(String user_name) throws Exception {
         Transacao tr = new Transacao();
         try {
             tr.beginReadOnly();
               UsuarioData cdata = new UsuarioData();
               UsuarioDO c = cdata.pesquisarPorLogin(user_name, tr);
+            tr.commit();
+            return c;
+        } catch (Exception e) {
+            tr.rollback();
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public MedicoDO buscarMedico(int idobj) throws Exception {
+        Transacao tr = new Transacao();
+        try {
+            tr.beginReadOnly();
+              MedicoDATA cdata = new MedicoDATA();
+              MedicoDO c = cdata.buscar(idobj, tr);
+            tr.commit();
+            return c;
+        } catch (Exception e) {
+            tr.rollback();
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public PacienteDO buscarPaciente(int idobj) throws Exception {
+        Transacao tr = new Transacao();
+        try {
+            tr.beginReadOnly();
+              PacienteDATA cdata = new PacienteDATA();
+              PacienteDO c = cdata.buscar(idobj, tr);
+            tr.commit();
+            return c;
+        } catch (Exception e) {
+            tr.rollback();
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public TecnicoDO buscarTecnico(int idobj) throws Exception {
+        Transacao tr = new Transacao();
+        try {
+            tr.beginReadOnly();
+              TecnicoDATA cdata = new TecnicoDATA();
+              TecnicoDO c = cdata.buscar(idobj, tr);
             tr.commit();
             return c;
         } catch (Exception e) {
